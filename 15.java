d016ce1ee9abb82f910fe4b8658c71120d621df9
jinxiaobo <xiaobodeworld@outlook.com>
2017-02-03 22:05:08 +0800

    文档复制

---
+++ 15.java
@@ -0,0 +1,20 @@
+/**
+ * Created by jinxiaobo on 17/1/31.
+ * 编程题：
+ 把一个文档中的内容拷贝到另一个文档中去
+ */
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        FileReader fr=new FileReader("lib/info.txt");
+        FileWriter fw=new FileWriter("lib/copy_info.txt");
+        int len=0;
+        while((len=fr.read())!=-1)
+            fw.write(len);
+        fr.close();
+        fw.close();
+    }
+}
